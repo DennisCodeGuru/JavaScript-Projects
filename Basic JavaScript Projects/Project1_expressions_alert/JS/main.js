dffafa2d4ae21d888dda960cf04d328451c051a8
DennisCodeGuru <denniskalmykovv@gmail.com>
2021-12-07 12:08:15 -0800

    js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t metter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+var B ="test";
+window.alert(B);
+var C = "Concatenated" + "String"
+document.write(C);
+var Fammily = "the Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Doughter = "Penny", Son = "Zorro";
+document.write(Dad)
+3+3
+document.write(3+3);
+
+function My_First_Function() {                               //Defining a function and naming it
+    var str = "This text is green!";                         //Defining a variable and giving it a
+                                                             //string value
+    var result = str.fontcolor("green");                     //Using the fontcolor method on
+                                                             //str value                         
+    document.getElementById("Green_Text").innerHTML = result;//Putting the value
+}                                                            //of result into HTML element "Green_Text" id
