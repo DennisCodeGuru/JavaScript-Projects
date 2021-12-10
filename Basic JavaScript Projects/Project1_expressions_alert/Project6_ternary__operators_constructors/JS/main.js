f50a1499e6e3b35b229b84999f68e9c4839eb3f0
DennisCodeGuru <denniskalmykovv@gmail.com>
2021-12-09 18:47:17 -0800

    Project6

--- Basic JavaScript Projects/Project1_expressions_alert/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/Project6_ternary__operators_constructors/JS/main.js
@@ -5,3 +5,28 @@ function Ride_Function() {
     Can_ride = (Age < 18) ? "You are not old enough":"You are old enough";
     document.getElementById("Vote").innerHTML = Can_ride + " to vote.";
 }
+//Keyword
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
+    " manufacured in " + Erik.Vehicle_Year;
+}
+//NESTED FUNCTIONS
+function Nested_Function() {
+    document.getElementById("Nested").innerHtml = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
